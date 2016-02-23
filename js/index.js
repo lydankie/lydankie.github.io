74cb4d3b211e5223a7bad2e9cb86dfb41aace511
Lauren Dankiewicz <lauren@betterfinance.me>
2016-02-22 23:10:01 -0800

    trying to format

--- js/index.js
+++ js/index.js
@@ -16,50 +16,250 @@ $(document).ready(function(){
       console.log(value["artist"])
       console.log(value.songName)
       if (userChineseLevel==value.chineseLevel && userGenre==value.genre) {
-        $('.song-result-list').append("<li><div class='song-result'> <img src="+value.albumImage+" alt='album image'><p>"+  value.artist +"</p><p>"+value.songName+"</p></div></li>");
+        $('.song-result-list').append("<li><div class='song-result'> <div class='image result-square'><img src="+value.albumImage+" alt='album image'></div><div class='result-description'><p>" + value.artistEnglish +"</p><p>"+value.artistMandarin+"</p><p>" + value.songNameEnglish +"</p><p>" + value.songNameMandarin + "</p><p>"+value.genre+"</div></div></li>");
       }
-      else {
-      }
+      // else {
+      //   $('.song-result-list').append("Sorry! I don't have any songs like this in my demo");
+      // }
 })
-
-
   });
-
-
-  }) //end doc ready
 
   var data = [
 
     {
-    artist: 'Chen Qi Zhen',
-    songName: 'Jealous ／ 嫉妒',
+    artistEnglish: 'Chen Qi Zhen',
+    artistMandarin: '陳綺貞',
+    songNameEnglish: 'Jealous',
+    songNameMandarin: '嫉妒',
     chineseLevel: 'Intermediate',
     albumImage: 'images/chenqizhen.png',
     genre: 'Pop'
     },
 
     {
-    artist: 'Jay Chou',
-    songName: 'White Windmill ／ 白色風車',
+    artistEnglish: 'Jay Chou',
+    artistMandarin: '周杰倫',
+    songNameEnglish: 'White Windmill',
+    songNameMandarin: '白色風車',
     chineseLevel: 'Beginner',
-    albumImage: 'images.jaychou.png',
+    albumImage: 'images/jaychou.png',
     genre: 'Pop'
     },
 
     {
-    artist: 'Teresa Teng',
-    songName: 'The Moon Represents My Heart / 月亮代表我的心',
+    artistEnglish: 'Teresa Teng',
+    artistMandarin: '鄧麗君',
+    songNameEnglish: 'The Moon Represents My Heart',
+    songNameMandarin: '月亮代表我的心',
     chineseLevel: 'Beginner',
-    albumImage: 'images.teresateng.png',
+    albumImage: 'images/teresateng.png',
     genre: 'Oldies'
-    },
+  },
 
-    {
-    artist: 'Chen Qi Zhen',
-    songName: 'Jealous ／ 嫉妒',
-    chineseLevel: 'Intermediate',
-    albumImage: 'images/chenqizhen.png',
-    genre: 'Pop'
-    }
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  },
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  },
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  },
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  },
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  },
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  },
+  {
+  artistEnglish: 'Chen Qi Zhen',
+  artistMandarin: '陳綺貞',
+  songNameEnglish: 'Jealous',
+  songNameMandarin: '嫉妒',
+  chineseLevel: 'Intermediate',
+  albumImage: 'images/chenqizhen.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Jay Chou',
+  artistMandarin: '周杰倫',
+  songNameEnglish: 'White Windmill',
+  songNameMandarin: '白色風車',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/jaychou.png',
+  genre: 'Pop'
+  },
+
+  {
+  artistEnglish: 'Teresa Teng',
+  artistMandarin: '鄧麗君',
+  songNameEnglish: 'The Moon Represents My Heart',
+  songNameMandarin: '月亮代表我的心',
+  chineseLevel: 'Beginner',
+  albumImage: 'images/teresateng.png',
+  genre: 'Oldies'
+  }
 
 ]
+
+  }) //end doc ready
